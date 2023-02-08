caf39e3178a677e27900db3f37a96c1d598befea
Faolan <46481567+faolan-rad@users.noreply.github.com>
2023-02-08 10:59:11 +0100

    LibGodot with GDExtension
    Co-Authored-By: K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
    
    (cherry picked from commit b2b25f9df25801e3c03131af247fcdfa40e588ac)

--- platform/linuxbsd/godot_linuxbsd.cpp
+++ platform/linuxbsd/godot_linuxbsd.cpp
@@ -41,6 +41,9 @@
 #include <sys/resource.h>
 #endif
 
+#include "main/main.h"
+#include "os_linuxbsd.h"
+
 int main(int argc, char *argv[]) {
 #if defined(SANITIZERS_ENABLED)
 	// Note: Set stack size to be at least 30 MB (vs 8 MB default) to avoid overflow, address sanitizer can increase stack usage up to 3 times.
